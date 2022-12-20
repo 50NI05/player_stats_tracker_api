50709c893b5bed42edda5ca9e0b480e3ed27fd0f
jonathan836 <jonathan999aaa@gmail.com>
2022-12-20 12:05:20 -0400

    ...

--- db/database.sql
+++ db/database.sql
@@ -7,17 +7,15 @@ CREATE TABLE t_user (
   firstName VARCHAR(45) DEFAULT NULL,
   lastName VARCHAR(45) DEFAULT NULL,
   email VARCHAR(100) DEFAULT NULL,
-  password VARCHAR(25) DEFAULT NULL,
+  password VARCHAR(150) DEFAULT NULL,
+  profile INT(100) DEFAULT 1,
   PRIMARY KEY (id)
 );
 
 DESCRIBE t_user;
 
 INSERT INTO t_user VALUES 
-  (1, 'Joe', 'Tesla', 'tesla@gmail.com', '1234'),
-  (2, 'Henry', 'Walker', 'walker@gmail.com', '1234'),
-  (3, 'Sam', 'Smith', 'smith@gmail.com', '1234'),
-  (4, 'Max', 'Alvaro', 'alvaro@gmail.com', '1234');
-
-ALTER TABLE t_user
-ADD COLUMN profile INT(10) DEFAULT NULL;
+  (1, 'Joe', 'Tesla', 'tesla@gmail.com', '1234', 1),
+  (2, 'Henry', 'Walker', 'walker@gmail.com', '1234', 1),
+  (3, 'Sam', 'Smith', 'smith@gmail.com', '1234', 1),
+  (4, 'Max', 'Alvaro', 'alvaro@gmail.com', '1234', 1);

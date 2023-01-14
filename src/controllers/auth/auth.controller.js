922a409971f365d11e77b2419def5812e091591c
jonathan836 <jonathan999aaa@gmail.com>
2023-01-14 19:03:31 -0400

    fix login 14/01/2023

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -7,70 +7,60 @@ export const logIn = async (req, res) => {
   const email = body.email;
   const password = body.password;
 
-  const [session] = await pool.query('SELECT * FROM t_user WHERE t_user.email = ?', [email])
+  try {
+    const [users] = await pool.query('SELECT * FROM t_user WHERE t_user.email = ?', [email])
 
-  if (session[0].token !== null) {
-    res.json({
-      status: 'ERROR',
-      // data: 'User already logged in.'
-      data: 'El usuario ya ha iniciado sesión.'
-    });
-  } else {
-    try {
-      const [users] = await pool.query('SELECT * FROM t_user WHERE t_user.email = ?', [email])
+    if (users.length === 0) {
+      res.json({
+        status: 'Error',
+        // data: 'Incorrect Email! Please try again'
+        data: 'Correo electrónico incorrecto Por favor, inténtelo de nuevo'
+      })
+    } else {
+      const validatePassword = await bcryptjs.compare(password, users[0].password)
 
-      if (users.length === 0) {
+      if (!validatePassword) {
         res.json({
           status: 'Error',
-          // data: 'Incorrect Email! Please try again'
-          data: 'Correo electrónico incorrecto Por favor, inténtelo de nuevo'
+          // data: 'Incorrect Password! Please try again'
+          data: 'Contraseña incorrecta Por favor, inténtelo de nuevo'
         })
       } else {
-        const validatePassword = await bcryptjs.compare(password, users[0].password)
+        const token = await generateJWT(users[0].id, users[0].firstName, users[0].lastName, users[0].email, users[0].password, users[0].profile)
 
-        if (!validatePassword) {
+        if (token.length === 0) {
           res.json({
             status: 'Error',
-            // data: 'Incorrect Password! Please try again'
-            data: 'Contraseña incorrecta Por favor, inténtelo de nuevo'
+            data: 'Error token'
           })
         } else {
-          const token = await generateJWT(users[0].id, users[0].firstName, users[0].lastName, users[0].email, users[0].password, users[0].profile)
+          const [rows] = await pool.query('UPDATE t_user SET token = ? WHERE id = ?', [token, users[0].id])
 
-          if (token.length === 0) {
-            res.json({
-              status: 'Error',
-              data: 'Error token'
+          if (rows.affectedRows > 0) {
+            res.send({
+              status: 'SUCCESS',
+              data: token,
+              id: users[0].id,
+              profile: users[0].profile,
+              email: users[0].email
             })
           } else {
-            const [rows] = await pool.query('UPDATE t_user SET token = ? WHERE id = ?', [token, users[0].id])
-
-            if (rows.affectedRows > 0) {
-              res.send({
-                status: 'SUCCESS',
-                data: token,
-                id: users[0].id,
-                profile: users[0].profile,
-                email: users[0].email
-              })
-            } else {
-              res.send({
-                status: 'ERROR',
-                // data: 'Error insert token'
-                data: 'Error al insertar token'
-              })
-            }
+            res.send({
+              status: 'ERROR',
+              // data: 'Error insert token'
+              data: 'Error al insertar token'
+            })
           }
         }
       }
-
-    } catch (error) {
-      res.status(500).json({
-        status: 'Error',
-        // data: 'An error has occurred'
-        data: 'Ha ocurrido un error'
-      })
     }
+
+  } catch (error) {
+    res.status(500).json({
+      status: 'Error',
+      // data: 'An error has occurred'
+      data: 'Ha ocurrido un error'
+    })
   }
 }
 

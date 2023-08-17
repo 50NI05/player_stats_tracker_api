98801ed5a6512eb2335daca15d2236374316e58e
zenTCS <jonathan.zheng@tcs.com.ve>
2023-08-17 09:38:58 -0400

    getAllSquad Service

--- src/controllers/football-players/squad.controller.js
+++ src/controllers/football-players/squad.controller.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Player } from "../../db.js";
+import { Player, Team } from "../../db.js";
 
 // export const squads = (req, res, next) => {
 //   const { team } = req.params
@@ -32,19 +32,45 @@ export const getSquad = async (req, res, next) => {
   const id = req.params.id
 
   try {
-    const player = await Player.findAll({ where: { id_team: id } });
+    const player = await Player.findAll({ where: { id_team: id }, include: Team });
     const formattedPlayers = player.map(e => ({
       id: e.id,
       name: e.name,
       age: e.age,
       number: e.number,
-      photo: e.photo
+      photo: e.photo,
+      team: e.t_team
     }))
 
     if (player) {
       res.status(200).json({
         status: 'SUCCESS',
         data: formattedPlayers
+      })
+    } else {
+      res.status(500).json({
+        status: 'ERROR',
+        data: 'Ocurrió un error al cargar los detalles del equipo.'
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 'ERROR',
+      // data: 'Something goes wrong'
+      data: 'Algo va mal'
+    })
+  }
+}
+
+export const getAllSquad = async (req, res) => {
+  try {
+    const player = await Player.findAll({ include: Team });
+
+    if (player) {
+      res.status(200).json({
+        status: 'SUCCESS',
+        data: player
       })
     } else {
       res.status(500).json({

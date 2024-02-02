c211f896349d1d0e0b577609aa945eb8dd61a1ee
jonathan836 <jonathan999aaa@gmail.com>
2024-02-02 16:55:27 -0400

    ...

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ import NewsAPI from 'newsapi';
 
 dotenv.config()
 
-export const PORT = process.env.PORT || 'LOCAL'
-export const DB_USER = process.env.DB_USER || 'LOCAL'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'LOCAL'
+export const PORT = process.env.PORT || 4000
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '9262865472'
 export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'LOCAL'
-export const DB_PORT = process.env.DB_PORT || 'LOCAL'
+export const DB_DATABASE = process.env.DB_DATABASE || 'playerStatsTrackerDB'
+export const DB_PORT = process.env.DB_PORT || 3306
 
 export const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
